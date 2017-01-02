b26672bca1203ded9914389aba56953108f2a4a8
KUSIO-DESKTOP\jduch <j.duchniewicz@gmail.com>
2017-01-02 17:57:53 +0100

    messed up pointers to barrel while refactoring

--- Battle Tank/BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ Battle Tank/BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -5,11 +5,20 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h" 
 
+//Enum for reloading state
+UENUM()
+enum class EFiringStatus : uint8
+{
+	Reloading,
+	Aiming,
+	Locked
+};
+
 //Forward declaration
 class UTankBarrel;
 class UTankTurret;
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankAimingComponent : public UActorComponent
 {
 	GENERATED_BODY()
@@ -18,12 +27,15 @@ public:
 	// Sets default values for this component's properties
 	UTankAimingComponent();
 
-	void SetBarrelReference(UTankBarrel* BarrelToSet);
-	void SetTurretReference(UTankTurret* TurretToSet);
-
 	void AimAt(FVector HitLocation, float LaunchSpeed); 
-private:
+	UFUNCTION(BlueprintCallable, Category = Setup)
+		void Initialise(UTankBarrel* BarrelToSet, UTankTurret* TurretToSet);
 	UTankBarrel* Barrel = nullptr;
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = State)
+		EFiringStatus FiringState = EFiringStatus::Reloading;
+private:
+	
 	UTankTurret* Turret = nullptr;
 
 	void MoveBarrelTowards(FVector AimDirection);
--- Battle Tank/BattleTank/Source/BattleTank/Tank.h
+++ Battle Tank/BattleTank/Source/BattleTank/Tank.h
@@ -18,10 +18,6 @@ class BATTLETANK_API ATank : public APawn
 	GENERATED_BODY()
 
 public:
-	UFUNCTION(BlueprintCallable, Category = Setup)
-	void SetBarrelReference(UTankBarrel* BarrelToSet);
-	UFUNCTION(BlueprintCallable, Category = Setup)
-	void SetTurretReference(UTankTurret* TurretToSet);
 	void AimAt(FVector HitLocation);
 	UFUNCTION(BlueprintCallable, Category = Firing)
 	void Fire();
@@ -37,10 +33,6 @@ private:
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
 
 	UPROPERTY(EditDefaultsOnly, Category = Setup)
 		TSubclassOf<AProjectile> ProjectileBlueprint;
