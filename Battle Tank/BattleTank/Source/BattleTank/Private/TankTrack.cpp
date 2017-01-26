18ed8f4c0c0c3fe7d07ea4b81e23fce5a249100e
KUSIO-DESKTOP\jduch <j.duchniewicz@gmail.com>
2017-01-26 19:35:53 +0100

    Added sideways friction

--- Battle Tank/BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ Battle Tank/BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -3,6 +3,34 @@
 #include "BattleTank.h"
 #include "TankTrack.h"
 
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankTrack::BeginPlay()
+{
+	Super::BeginPlay();
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit); // should be OnHit?
+}
+
+void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	auto SlippageSpeed = FVector::DotProduct(GetComponentVelocity(), GetRightVector());
+	//Work out the required acceleration this frame to correct
+	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime*GetRightVector();
+
+	//Calculate and apply sideways F = m * a
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto CorrectionForce = (TankRoot->GetMass()) * CorrectionAcceleration/2; //divide by 2 because we have 2 tracks
+	TankRoot->AddForce(CorrectionForce);
+}
+
+void UTankTrack::OnHit(UPrimitiveComponent * HitComponent, AActor * OtherActor, UPrimitiveComponent * OtherComponent, FVector NormalImpulse, const FHitResult & Hit)
+{
+	UE_LOG(LogTemp,Warning, TEXT("HITS"))
+}
+
 void UTankTrack::SetThrottle(float Throttle)
 {
 	auto ForceApllied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
--- Battle Tank/BattleTank/Source/BattleTank/Public/TankTrack.h
+++ Battle Tank/BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -21,4 +21,12 @@ public:
 	//Max force per track in Newtons
 	UPROPERTY(EditDefaultsOnly)
 		float TrackMaxDrivingForce = 40000000.0; //assume 40t tank and 1g acceleration 
+private:
+	UTankTrack();
+	virtual void BeginPlay() override;
+
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+
 };
